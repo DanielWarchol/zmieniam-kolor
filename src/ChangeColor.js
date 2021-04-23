93c7358156992612564a71bc20c4313dd11ecb37
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-04-23 15:26:23 +0200

    zmieniam-kolor

--- src/ChangeColor.js
+++ src/ChangeColor.js
@@ -4,16 +4,17 @@ import  './ChangeColor.css';
 
 function ChangeColor() {
 
-    const [textColor, setTextColor] = useState('text-color-green');
+    const [textColor, setTextColor] = useState('text-color-black');
+    const [inputText , setInputText] = useState('');
 
     return (
         <div className="color-main">
             <h1 className={textColor}>Zmieniam Kolor</h1>
             
+            <input type="text" className="text"></input><br />
             <button className="btn" onClick={()=>setTextColor('text-color-red')}>red</button>
             <button className="btn" onClick={()=>setTextColor('text-color-green')}>green</button>
             <button className="btn" onClick={()=>setTextColor('text-color-blue')}>blue</button>
-            
         </div>
     )
 }
